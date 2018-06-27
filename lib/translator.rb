d839055eb0b563f1c75d01a3f7d9d6bf3388964f
Yan Wang <wangyan6783@gmail.com>
2018-06-27 11:57:45 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(path, emoticon)
   if translator[emoticon]
     translator[emoticon]
   else
-    puts "Sorry, that emoticon was not found"
+    "Sorry, that emoticon was not found"
   end
 end
 
