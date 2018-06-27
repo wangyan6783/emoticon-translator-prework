7a268a4071c629db6857b6b8bd57e105fa2b03eb
Yan Wang <wangyan6783@gmail.com>
2018-06-27 11:43:10 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,46 @@ def load_library(path)
   emoticons
 end
 
+
+it "the keys inside the 'get_meaning' hash are the Japanese emoticons" do
+      emoticons = ["☜(⌒▽⌒)☞", "(ΘεΘ;)", "(￣ー￣)", "o_O", "(^_-)"]
+      emoticons.each do |japanese_emoticon| 
+        expect(result['get_meaning'].keys).to include(japanese_emoticon)
+      end
+    end
+
+    it "the emoticon keys inside the 'get_meaning' hash point to their meanings" do
+      emoticons = {
+        "☜(⌒▽⌒)☞" => "angel",
+        "(ΘεΘ;)" => "bored",
+        "o_O" => "surprised",
+        "(^_-)" => "wink"
+      }
+      emoticons.each do |japanese_emoticon,meaning| 
+        expect(result['get_meaning'][japanese_emoticon]).to eq(meaning)
+      end
+    end
+
+    it "the keys inside the 'get_emoticon' hash are the English emoticons" do
+      emoticons = ["O:)", ":'(", ";)", ":$"]
+      emoticons.each do |english_emoticon| 
+        expect(result['get_emoticon'].keys).to include(english_emoticon)
+      end
+    end
+
+    it "the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents" do
+      emoticons = {"O:)" => "☜(⌒▽⌒)☞", ":'(" => "(Ｔ▽Ｔ)", ";)" => "(^_-)"}
+      emoticons.each do |english_emoticon,japanese_emoticon| 
+        expect(result['get_emoticon'][english_emoticon]).to eq(japanese_emoticon)
+      end
+    end
+
+  end
+
+end
+
+
+-------------------------
 def get_japanese_emoticon
   # code goes here
 end
